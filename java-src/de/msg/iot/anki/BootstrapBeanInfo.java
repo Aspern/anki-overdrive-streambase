e40a8837f8d3298a2f2e852086a108ee070bbef6
Adrian Weber <adrian@kuebler-weber.de>
2017-04-04 20:07:33 +0200

    added auto start (Bootstrap)

---
+++ java-src/de/msg/iot/anki/BootstrapBeanInfo.java
@@ -0,0 +1,25 @@
+package de.msg.iot.anki;
+
+import java.beans.*;
+
+import com.streambase.sb.operator.parameter.*;
+
+/**
+ * A BeanInfo class controls what properties are exposed, add metadata about
+ * properties (such as which properties are optional), and access special types
+ * of properties that can't be automatically derived via reflection. If a
+ * BeanInfo class is present, only the properties explicitly declared in this
+ * class will be exposed by StreamBase.
+ */
+public class BootstrapBeanInfo extends SBSimpleBeanInfo {
+
+	/*
+	 * The order of properties below determines the order they are displayed
+	 * within the StreamBase Studio property view.
+	 */
+	public SBPropertyDescriptor[] getPropertyDescriptorsChecked()
+			throws IntrospectionException {
+		return new SBPropertyDescriptor[] {};
+	}
+
+}
